100bd8a18eb0defa6bc8b291ed2b9e0120520d4e
linxinzhe <linxinzhechn@163.com>
2016-05-28 18:10:52 +0800

    feature(other): add a view with drawableLeft and text together center in view

---
+++ app/src/main/java/com/linxinzhe/android/codebaseapp/view/DrawableCenterTextView.java
@@ -0,0 +1,40 @@
+package com.linxinzhe.android.codebaseapp.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * @author linxinzhe on 5/28/16.
+ */
+public class DrawableCenterTextView extends TextView {
+
+    public DrawableCenterTextView(Context context) {
+        super(context);
+    }
+
+    public DrawableCenterTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public DrawableCenterTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Drawable[] drawables = getCompoundDrawables();
+        Drawable drawableLeft = drawables[0];
+        if (drawableLeft != null) {
+            float textWidth = getPaint().measureText(getText().toString());
+            int drawablePadding = getCompoundDrawablePadding();
+            int drawableWidth = drawableLeft.getIntrinsicWidth();
+            float bodyWidth = textWidth + drawableWidth + drawablePadding;
+            canvas.translate((getWidth() - bodyWidth) / 2, 0);
+        }
+        super.onDraw(canvas);
+    }
+}
+
