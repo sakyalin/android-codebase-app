6f5dd27211f629c5d3356997030eeabddec23e8c
linxinzhe <linxinzhechn@163.com>
2016-06-05 21:12:09 +0800

    feature(main):  keyboard will disappear when click other place

--- app/src/main/java/com/linxinzhe/android/codebaseapp/feature/MainActivity.java
+++ app/src/main/java/com/linxinzhe/android/codebaseapp/feature/MainActivity.java
@@ -1,12 +1,16 @@
 package com.linxinzhe.android.codebaseapp.feature;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.linxinzhe.android.codebaseapp.R;
@@ -68,6 +72,48 @@ public class MainActivity extends BaseActivity {
     @OnClick(R.id.btn_go_upload_img)
     public void goUploadImg(View view) {
         startActivity(UploadImgActivity.class);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        //点击其它区域则键盘隐藏
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (isShouldHideInput(v, ev)) {
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+            return super.dispatchTouchEvent(ev);
+        }
+        // 必不可少，否则所有的组件都不会有TouchEvent了
+        if (getWindow().superDispatchTouchEvent(ev)) {
+            return true;
+        }
+        return onTouchEvent(ev);
+    }
+
+    public boolean isShouldHideInput(View v, MotionEvent event) {
+        //点击其它区域则键盘隐藏
+        if (v != null && (v instanceof EditText)) {
+            int[] leftTop = {0, 0};
+            //获取输入框当前的location位置
+            v.getLocationInWindow(leftTop);
+            int left = leftTop[0];
+            int top = leftTop[1];
+            int bottom = top + v.getHeight();
+            int right = left + v.getWidth();
+            if (event.getX() > left && event.getX() < right
+                    && event.getY() > top && event.getY() < bottom) {
+                // 点击的是输入框区域，保留点击EditText的事件
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 
     private boolean isOnKeyBacking;
